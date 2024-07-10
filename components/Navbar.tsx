b4d1be17266a83a6801faf8987363718a01e6936
Mahmut Can <131211899+macasnet@users.noreply.github.com>
2024-07-10 13:46:09 +0300

    navbar_complated_update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar: React.FC = () => {
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         {/* Sol taraf - Marka Adı */}
         <div className="flex items-center">
-          <Link href="/" passHref>
+          <Link href="/" passHref legacyBehavior>
             <span className="text-black text-3xl font-thin cursor-pointer">
               CanDEV<span className="text-red-500">.com</span>
             </span>
@@ -62,11 +62,11 @@ const Navbar: React.FC = () => {
           {/* isOpen durumuna göre menüyü göster veya gizle */}
           {isOpen && (
             <div
-              className="absolute top-full left-0 bg-white border border-gray-600 mt-2 rounded-lg shadow-lg z-10"
+              className="absolute top-full right-0 bg-white border border-gray-600 mt-2 rounded-lg shadow-lg z-10"
               style={{ minWidth: '200px' }}
             >
               <div className="flex flex-col p-4 space-y-2">
-                <Link href="/blog" passHref>
+                <Link href="/blog" passHref legacyBehavior>
                   <button
                     onClick={toggleMenu}
                     className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300 w-full"
@@ -74,7 +74,7 @@ const Navbar: React.FC = () => {
                     Blog
                   </button>
                 </Link>
-                <Link href="/projects" passHref>
+                <Link href="/projects" passHref legacyBehavior>
                   <button
                     onClick={toggleMenu}
                     className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300 w-full"
@@ -82,7 +82,7 @@ const Navbar: React.FC = () => {
                     Projects
                   </button>
                 </Link>
-                <Link href="/about" passHref>
+                <Link href="/about" passHref legacyBehavior>
                   <button
                     onClick={toggleMenu}
                     className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300 w-full"
@@ -97,20 +97,29 @@ const Navbar: React.FC = () => {
 
         {/* Büyük ekranlarda normal menü */}
         <div className="hidden md:flex space-x-4">
-          <Link href="/blog" passHref>
-            <a className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300">
+          <Link
+            href="/blog"
+            passHref
+            className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300">
+            
               Blog
-            </a>
+            
           </Link>
-          <Link href="/projects" passHref>
-            <a className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300">
+          <Link
+            href="/projects"
+            passHref
+            className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300">
+            
               Projects
-            </a>
+            
           </Link>
-          <Link href="/about" passHref>
-            <a className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300">
+          <Link
+            href="/about"
+            passHref
+            className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300">
+            
               About
-            </a>
+            
           </Link>
         </div>
       </div>
