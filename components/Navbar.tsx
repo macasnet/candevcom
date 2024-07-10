d8a72f1195dd286e0d8b590c1c8b415aa272cade
Mahmut Can <131211899+macasnet@users.noreply.github.com>
2024-07-10 13:33:05 +0300

    navbar_hmbrgr_update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar: React.FC = () => {
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         {/* Sol taraf - Marka Adı */}
         <div className="flex items-center">
-          <Link href="/">
+          <Link href="/" passHref>
             <span className="text-black text-3xl font-thin cursor-pointer">
               CanDEV<span className="text-red-500">.com</span>
             </span>
@@ -25,15 +25,47 @@ const Navbar: React.FC = () => {
         {/* Sağ taraf - Hamburger Menü ve Dropdown Menü */}
         <div className="md:hidden">
           <button
+            type="button"
             onClick={toggleMenu}
-            className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300"
+            className="text-black md:hidden bg-white px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300"
           >
-            ☰ {/* Hamburger icon or any other icon for menu */}
+            {isOpen ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              </svg>
+            )}
           </button>
+          {/* isOpen durumuna göre menüyü göster veya gizle */}
           {isOpen && (
-            <div className="absolute top-full left-0 right-0 bg-white border border-gray-600 mt-1 rounded-lg shadow-lg">
+            <div className="absolute top-full left-0 right-0 bg-white border border-gray-600 mt-1 rounded-lg shadow-lg z-10">
               <div className="flex flex-col p-4 space-y-2">
-                <Link href="/blog">
+                <Link href="/blog" passHref>
                   <span
                     onClick={toggleMenu}
                     className="text-black hover:text-gray-800 cursor-pointer"
@@ -42,7 +74,7 @@ const Navbar: React.FC = () => {
                   </span>
                 </Link>
                 
-                <Link href="/projects">
+                <Link href="/projects" passHref>
                   <span
                     onClick={toggleMenu}
                     className="text-black hover:text-gray-800 cursor-pointer"
@@ -50,7 +82,7 @@ const Navbar: React.FC = () => {
                     Projects
                   </span>
                 </Link>
-                <Link href="/about">
+                <Link href="/about" passHref>
                   <span
                     onClick={toggleMenu}
                     className="text-black hover:text-gray-800 cursor-pointer"
@@ -65,22 +97,22 @@ const Navbar: React.FC = () => {
 
         {/* Büyük ekranlarda normal menü */}
         <div className="hidden md:flex space-x-4">
-          <Link href="/blog">
-            <button className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300">
+          <Link href="/blog" passHref>
+            <a className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300">
               Blog
-            </button>
+            </a>
           </Link>
           
-          <Link href="/projects">
-            <button className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300">
+          <Link href="/projects" passHref>
+            <a className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300">
               Projects
-            </button>
+            </a>
           </Link>
 
-          <Link href="/about">
-            <button className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300">
+          <Link href="/about" passHref>
+            <a className="bg-white text-black px-4 py-2 rounded-lg border border-gray-600 hover:bg-gray-300">
               About
-            </button>
+            </a>
           </Link>
         </div>
         
